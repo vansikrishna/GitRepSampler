495914987b30c6a1685f8b336040ede3e24a043d
vansikrishna <bansi.prem@gmail.com>
2017-10-11 15:10:14 -0400

    test submission final

--- mylibrary/src/main/java/com/gitrep/mylibrary/DecodeJson.java
+++ mylibrary/src/main/java/com/gitrep/mylibrary/DecodeJson.java
@@ -1,6 +1,5 @@
 package com.gitrep.mylibrary;
 
-import android.support.annotation.Keep;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -8,7 +7,7 @@ import com.google.gson.Gson;
 /**
  * Created by c029312 on 10/11/17.
  */
-@Keep
+
 public class DecodeJson {
 
     public static Object decode(String json, Class clazzName) throws Exception{
